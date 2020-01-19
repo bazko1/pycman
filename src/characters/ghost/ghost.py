59ebefb473e919a677e42f41dfadb55e70b8299c
Zin <znajer@gmail.com>
2020-01-19 13:08:21 +0100

    Ghosts implementation

--- src/characters/ghost/ghost.py
+++ src/characters/ghost/ghost.py
@@ -29,7 +29,9 @@ class Ghost(Movable):
         min_dist = float("inf")
         min_index = 0
         for i in range(len(step_x)):
-            if not self.backward_move(step_x[i], step_y[i]) or backward:
+            if (not self.backward_move(step_x[i], step_y[i]) or backward) and not self.board.is_wall(self.x + step_x[i],
+                                                                                                     self.y + step_y[
+                                                                                                         i]):
                 dist = (self.x + step_x[i] - self.target_x) ** 2 + (self.y + step_y[i] - self.target_y) ** 2
                 if dist < min_dist:
                     min_dist = dist
@@ -42,10 +44,10 @@ class Ghost(Movable):
     def step(self):
         self.prev_x, self.prev_y = self.x, self.y
         self.set_velocity()
-        
+
         newX = self.x + self.x_vel
         newY = self.y + self.y_vel
-        
+
         if not self.board.is_wall(newX, newY):
             self.x, self.y = newX, newY
 
